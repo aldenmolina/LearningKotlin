1d17f2b11c96d036acf4998e4ca79e46c7ae84a9
Alden <aj.molina092@gmail.com>
2019-02-18 22:37:47 -0500

    specified data types when declaring and initializing variables

--- app/src/main/java/com/example/myapplication/LearningKotlin.kt
+++ app/src/main/java/com/example/myapplication/LearningKotlin.kt
@@ -2,24 +2,22 @@ package com.example.myapplication
 
 fun main(args:Array<String>){
 
-    var age = 29;
-
-    var candy = "Chocolate"
-
-    println(age)
-    println(candy)
-
-    candy = "Snickers"
-
-    println(candy)
-
-    var weather = "Mostly Cloudy"
+    var age = 29
+    var candy = "Snickers"
+    var weather = "Mostly cloudy"
 
     println(weather)
 
+//showing and declaring types
 
+    var year : Int = 2019
+    var month : String = "February"
 
-
+    // variable can be declared but needs to
+    // be initialized with a value
+    var day : Int
+    day = 20
+    
 
 
 }
